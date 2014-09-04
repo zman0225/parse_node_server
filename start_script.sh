6263d82de41f4f10062597d013e17bdea54decfe
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-09-04 16:31:14 -0400

    edits

--- start_script.sh
+++ start_script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#setup ssl 
+if [! -f ssl/key.cem | ! -f ssl/cert.cem]; then
+	#statements
+	echo "SSL PEMs/folder not found, generating new key/cert"
+	cd util
+	./gen_ssl_cert.sh
+	cd ..
+fi
+
 sudo pm2 startup ubuntu
 sudo pm2 start processes.json
 sudo pm2 save
