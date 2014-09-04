fa1a9b7982e2556ae314ec3136b30204fec66d69
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-09-04 13:38:21 -0400

    edits

--- install_script.sh
+++ install_script.sh
@@ -3,13 +3,13 @@
 #install mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
-sudo apt-get install mongodb-org
+sudo apt-get -y update
+sudo apt-get -y install mongodb-org
 sudo service mongod restart
 
 #install pm2
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
 npm install
 npm install pm2@latest -g
 
