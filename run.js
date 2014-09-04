13c8c83d4f21eac8d21285005d36997ec1e3402b
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-09-04 12:54:04 -0400

    edits

--- run.js
+++ run.js
@@ -2,13 +2,14 @@
 "use strict";
 
 require("./datakit").run({
+	"port":80,
   "secret": "c821a09ebf01e090a46b6bbe8b21bcb36eb5b432265a51a76739c20472908989",
   "salt": "cfgsalt",
   'allowDestroy': true,
   'allowDrop': true,
-  'productionMode':true,
-  // 'push_key':"./push_development_ssl/key.pem",
-  // 'push_cert':"./push_development_ssl/cert.pem",
-   'push_key':"./push_production_ssl/key.pem",
-   'push_cert':"./push_production_ssl/cert.pem",
+  'productionMode':false,
+  'push_key':"./push_development_ssl/key.pem",
+  'push_cert':"./push_development_ssl/cert.pem",
+   // 'push_key':"./push_production_ssl/key.pem",
+   // 'push_cert':"./push_production_ssl/cert.pem",
 });
