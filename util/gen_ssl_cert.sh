3152bb809b030dffcd978308364e8c33265c945a
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-09-04 16:22:26 -0400

    edits

--- util/gen_ssl_cert.sh
+++ util/gen_ssl_cert.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 mkdir ../ssl
-sudo openssl req -new -x509 -days 365 -nodes -out ../ssl/cert.pem -keyout ../ssl/cert.pem
+# sudo openssl genrsa -des3 -out ../ssl/www.api.grid.social.key 1024
+# sudo openssl req -new -key ../ssl/www.api.grid.social.key -out ../ssl/www.api.grid.social.csr
+sudo openssl req -new -x509 -days 365 -nodes -out ../ssl/cert.pem -keyout ../ssl/key.pem
