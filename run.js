d83b541ee6776144a973fb3f36b601f692cfec53
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-09-04 19:27:37 -0400

    edits

--- run.js
+++ run.js
@@ -4,7 +4,7 @@
 require("./datakit").run({
   "key":"./ssl/key.pem",
   "cert":"./ssl/cert.pem",
-	"port":443,
+	"port":8000,
   "secret": "c821a09ebf01e090a46b6bbe8b21bcb36eb5b432265a51a76739c20472908989",
   "salt": "cfgsalt",
   'allowDestroy': true,
